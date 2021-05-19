3f7cb3b990eac24250a09199b094f5cac87cd623
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-05-19 17:47:33 -0300

    entrega

---
+++ READ.md
@@ -0,0 +1,3 @@
+# API BACKEND
+# Link do POSTMAN com as requisições 
+https://www.getpostman.com/collections/b0d3f4512adde066077d
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var cors = require('cors')
-const connectDB = require('./config/db')
 const app = express()
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000
 
 // Middleware
